e446b2225ecc26470c29b384b7ce9a3893a67515
ktanaka101 <kentanaka101@gmail.com>
2020-06-14 23:41:41 +0900

    Add conditionals for opcode

---
+++ src/vm/opcode/jump.rs
@@ -0,0 +1,21 @@
+use super::preludes::*;
+
+#[derive(Debug)]
+pub struct Jump(pub u16);
+
+impl OperandCode for Jump {
+    const TYPE: OperandType = OperandType::Jump;
+    const NAME: &'static str = "Jump";
+}
+
+impl Display for Jump {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {}", self.name(), self.0)
+    }
+}
+
+impl vm::convert::Read<u16, 2> for Jump {
+    fn read(bytes: [vm::bytecode::Instruction; 2]) -> u16 {
+        u16::from_be_bytes(bytes)
+    }
+}
---
+++ src/vm/opcode/jump_not_truthy.rs
@@ -0,0 +1,21 @@
+use super::preludes::*;
+
+#[derive(Debug)]
+pub struct JumpNotTruthy(pub u16);
+
+impl OperandCode for JumpNotTruthy {
+    const TYPE: OperandType = OperandType::JumpNotTruthy;
+    const NAME: &'static str = "JumpNotTruthy";
+}
+
+impl Display for JumpNotTruthy {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {}", self.name(), self.0)
+    }
+}
+
+impl vm::convert::Read<u16, 2> for JumpNotTruthy {
+    fn read(bytes: [vm::bytecode::Instruction; 2]) -> u16 {
+        u16::from_be_bytes(bytes)
+    }
+}
