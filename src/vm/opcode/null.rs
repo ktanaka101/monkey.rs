57e06aa7dd5252585e6a80eb5339664f94fa763a
ktanaka101 <kentanaka101@gmail.com>
2020-06-17 22:34:40 +0900

    Add Null for Opcode

---
+++ src/vm/opcode/null.rs
@@ -0,0 +1,21 @@
+use super::preludes::*;
+
+#[derive(Debug, Clone)]
+pub struct Null;
+
+impl OperandCode for Null {
+    const TYPE: OperandType = OperandType::Null;
+    const NAME: &'static str = "Null";
+}
+
+impl Display for Null {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.name())
+    }
+}
+
+impl vm::convert::Read<(), 0> for Null {
+    fn read(_: [vm::bytecode::Instruction; 0]) -> () {
+        ()
+    }
+}
