b1ebefc546d888139b871c92b9a2c2b253ace13a
ktanaka101 <kentanaka101@gmail.com>
2020-06-18 23:22:34 +0900

    Add SetGlobal, GetGlobal for opcode

---
+++ src/vm/opcode/set_global.rs
@@ -0,0 +1,27 @@
+use super::preludes::*;
+
+#[derive(Debug, Clone)]
+pub struct SetGlobal(pub u16);
+
+impl From<u16> for SetGlobal {
+    fn from(value: u16) -> Self {
+        SetGlobal(value)
+    }
+}
+
+impl Display for SetGlobal {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{} {}", self.name(), self.0)
+    }
+}
+
+impl OperandCode for SetGlobal {
+    const TYPE: OperandType = OperandType::SetGlobal;
+    const NAME: &'static str = "SetGlobal";
+}
+
+impl vm::convert::Read<u16, 2> for SetGlobal {
+    fn read(bytes: [vm::bytecode::Instruction; 2]) -> u16 {
+        u16::from_be_bytes(bytes)
+    }
+}
