14c4d809cf0d453d6ca5791537be04fd594b028a
ktanaka101 <kentanaka101@gmail.com>
2020-06-28 16:15:06 +0900

    Add index for opcode

---
+++ src/vm/opcode/index.rs
@@ -0,0 +1,21 @@
+use super::preludes::*;
+
+#[derive(Debug, Clone)]
+pub struct Index;
+
+impl OperandCode for Index {
+    const TYPE: OperandType = OperandType::Index;
+    const NAME: &'static str = "Index";
+}
+
+impl Display for Index {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.name())
+    }
+}
+
+impl vm::convert::Read<(), 0> for Index {
+    fn read(_: [vm::bytecode::Instruction; 0]) -> () {
+        ()
+    }
+}
