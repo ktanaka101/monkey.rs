b1e7b7614ae213765c01185ed9b5a32f035e9990
ktanaka101 <kentanaka101@gmail.com>
2021-12-04 16:42:09 +0900

    formatting

--- src/vm.rs
+++ src/vm.rs
@@ -460,7 +460,12 @@ impl<'a> VM<'a> {
     ) -> Result<objects::StringLit> {
         let value = match op {
             opcode::Opcode::Add(_) => left_val.to_string() + right_val,
-            _ => return Err(anyhow::format_err!("unknown string operator. received {}", op)),
+            _ => {
+                return Err(anyhow::format_err!(
+                    "unknown string operator. received {}",
+                    op
+                ))
+            }
         };
 
         Ok(objects::StringLit { value })
